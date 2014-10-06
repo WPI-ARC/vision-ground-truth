25d27c0ebb6a74c461ed2e59e31d814cc25caca6
Riley Larkins <rdlarkins@wpi.edu>
2014-10-06 10:11:41 -0400

    Changed colors, added debugging aides.

--- src/soft_robot_tracker.cpp
+++ src/soft_robot_tracker.cpp
@@ -16,6 +16,7 @@ struct trackingItem {
     int trackedPixels;
     cv::Scalar lowerBound;
     cv::Scalar upperBound;
+    cv::Scalar replace;
 };
 
 class SoftRobotTracker
@@ -56,7 +57,13 @@ public:
         }
     }
 
-    void checkColor(trackingItem *item, uint8_t red, uint8_t green, uint8_t blue, size_t i, size_t j) {
+    void checkColor(trackingItem *item, cv::Mat filtered, size_t i, size_t j) {
+        cv::Vec3b pixel = filtered.at<cv::Vec3b>(i,j);
+
+        uint8_t blue  = pixel[2];
+        uint8_t green = pixel[1];
+        uint8_t red   = pixel[0];
+
         if (blue >=  item->lowerBound[2] && blue <=  item->upperBound[2] &&
             green >= item->lowerBound[1] && green <= item->upperBound[1] &&
             red >=   item->lowerBound[0] && red <=   item->upperBound[0])
@@ -64,11 +71,16 @@ public:
             item->trackingMarker.x += (float)j;
             item->trackingMarker.y += (float)i;
             item->trackedPixels++;
+
+            pixel[2] = item->replace[2];
+            pixel[1] = item->replace[1];
+            pixel[0] = item->replace[0];
+            filtered.at<cv::Vec3b>(i,j) = pixel;
         }
     }
 
     void camera_cb(const sensor_msgs::ImageConstPtr& image) {
-        ROS_INFO("Got new image to resize and track");
+        // ROS_INFO("Got new image to resize and track");
         // Convert to OpenCV
         cv_bridge::CvImagePtr cv_ptr;
         try
@@ -93,74 +105,86 @@ public:
         struct trackingItem greenSpot;
         greenSpot.trackingMarker = cv::Point2f(0.0, 0.0);
         greenSpot.trackedPixels = 0;
-        greenSpot.lowerBound = cv::Scalar(0, 121, 0);
-        greenSpot.upperBound = cv::Scalar(120, 255, 120);
-
-        struct trackingItem redSpot;
-        redSpot.trackingMarker = cv::Point2f(0.0, 0.0);
-        redSpot.trackedPixels = 0;
-        redSpot.lowerBound = cv::Scalar(121, 0, 0);
-        redSpot.upperBound = cv::Scalar(255, 120, 120);
+        greenSpot.lowerBound = cv::Scalar(150, 180, 120);
+        greenSpot.upperBound = cv::Scalar(175, 220, 160);
+        greenSpot.replace    = cv::Scalar(0, 0, 0);
 
         struct trackingItem yellowSpot;
         yellowSpot.trackingMarker = cv::Point2f(0.0, 0.0);
         yellowSpot.trackedPixels = 0;
-        yellowSpot.lowerBound = cv::Scalar(121, 121, 0);
-        yellowSpot.upperBound = cv::Scalar(255, 255, 120);
+        yellowSpot.lowerBound = cv::Scalar(200, 200, 0);
+        yellowSpot.upperBound = cv::Scalar(255, 255, 140);
+        yellowSpot.replace    = cv::Scalar(0, 0, 0);
+
+        struct trackingItem orangeSpot;
+        orangeSpot.trackingMarker = cv::Point2f(0.0, 0.0);
+        orangeSpot.trackedPixels = 0;
+        orangeSpot.lowerBound = cv::Scalar(170, 130, 80);
+        orangeSpot.upperBound = cv::Scalar(230, 145, 130);
+        orangeSpot.replace    = cv::Scalar(0, 0, 0);
 
         struct trackingItem blueSpot;
         blueSpot.trackingMarker = cv::Point2f(0.0, 0.0);
         blueSpot.trackedPixels = 0;
-        blueSpot.lowerBound = cv::Scalar(0, 0, 121);
-        blueSpot.upperBound = cv::Scalar(120, 120, 255);
+        blueSpot.lowerBound = cv::Scalar(0, 0, 100);
+        blueSpot.upperBound = cv::Scalar(70, 70, 255);
+        blueSpot.replace    = cv::Scalar(0, 0, 0);
 
         // Because InRangeS is shit, use our own
         for (size_t i = 0; i < filtered.rows; i++)
         {
             for (size_t j = 0; j < filtered.cols; j++)
             {
-                cv::Vec3b pixel = filtered.at<cv::Vec3b>(i,j);
-                uint8_t blue = pixel[2];
-                uint8_t green = pixel[1];
-                uint8_t red = pixel[0];
 
-                checkColor(&greenSpot,  red, green, blue, i, j);
-                checkColor(&redSpot,    red, green, blue, i ,j);
-                checkColor(&yellowSpot, red, green, blue, i, j);
-                checkColor(&blueSpot,   red, green, blue, i, j);
+                checkColor(&greenSpot,  filtered, i, j);
+                checkColor(&yellowSpot, filtered, i ,j);
+                checkColor(&orangeSpot, filtered, i, j);
+                checkColor(&blueSpot,   filtered, i, j);
             }
         }
         // Now, compute the average x and y values for the tracked marker
         greenSpot.trackingMarker.x = greenSpot.trackingMarker.x / (float)greenSpot.trackedPixels;
         greenSpot.trackingMarker.y = greenSpot.trackingMarker.y / (float)greenSpot.trackedPixels;
-        ROS_INFO("Green tracking marker found with center %f (x) %f (y)", greenSpot.trackingMarker.x, greenSpot.trackingMarker.y);
-
-        redSpot.trackingMarker.x = redSpot.trackingMarker.x / (float)redSpot.trackedPixels;
-        redSpot.trackingMarker.y = redSpot.trackingMarker.y / (float)redSpot.trackedPixels;
-        ROS_INFO("Red tracking marker found with center %f (x) %f (y)", redSpot.trackingMarker.x, redSpot.trackingMarker.y);
+        //ROS_INFO("Green tracking marker found with center %f (x) %f (y)", greenSpot.trackingMarker.x, greenSpot.trackingMarker.y);
 
         yellowSpot.trackingMarker.x = yellowSpot.trackingMarker.x / (float)yellowSpot.trackedPixels;
         yellowSpot.trackingMarker.y = yellowSpot.trackingMarker.y / (float)yellowSpot.trackedPixels;
-        ROS_INFO("Yellow tracking marker found with center %f (x) %f (y)", yellowSpot.trackingMarker.x, yellowSpot.trackingMarker.y);
+        //ROS_INFO("Red tracking marker found with center %f (x) %f (y)", yellowSpot.trackingMarker.x, yellowSpot.trackingMarker.y);
+
+        orangeSpot.trackingMarker.x = orangeSpot.trackingMarker.x / (float)orangeSpot.trackedPixels;
+        orangeSpot.trackingMarker.y = orangeSpot.trackingMarker.y / (float)orangeSpot.trackedPixels;
+        //ROS_INFO("Yellow tracking marker found with center %f (x) %f (y)", orangeSpot.trackingMarker.x, orangeSpot.trackingMarker.y);
 
         blueSpot.trackingMarker.x = blueSpot.trackingMarker.x / (float)blueSpot.trackedPixels;
         blueSpot.trackingMarker.y = blueSpot.trackingMarker.y / (float)blueSpot.trackedPixels;
-        ROS_INFO("Blue tracking marker found with center %f (x) %f (y)", blueSpot.trackingMarker.x, blueSpot.trackingMarker.y);
+        //ROS_INFO("Blue tracking marker found with center %f (x) %f (y)", blueSpot.trackingMarker.x, blueSpot.trackingMarker.y);
+
+        // blue, yellow, orange, green
+        ROS_INFO(", %f, %f, ||, %f, %f, ||, %f, %f, ||, %f, %f",
+                 blueSpot.trackingMarker.x,   blueSpot.trackingMarker.y,
+                 yellowSpot.trackingMarker.x, yellowSpot.trackingMarker.y,
+                 orangeSpot.trackingMarker.x, orangeSpot.trackingMarker.y,
+                 greenSpot.trackingMarker.x,  greenSpot.trackingMarker.y);
+//        ROS_ERROR(", %f, %f, ||, %f, %f, ||, %f, %f, ||, %f, %f",
+//                 blueSpot.trackingMarker.x,   blueSpot.trackingMarker.y,
+//                 yellowSpot.trackingMarker.x, yellowSpot.trackingMarker.y,
+//                 orangeSpot.trackingMarker.x, orangeSpot.trackingMarker.y,
+//                 greenSpot.trackingMarker.x,  greenSpot.trackingMarker.y);
 
         /////////////////////////////////////////////////////
         ///// Publish the debug image /////
         /////////////////////////////////////////////////////
         // Make the debug image with checkboard centers, actuator base, and actuator tip drawn in
         cv::circle(filtered, cv::Point2i(snap(greenSpot.trackingMarker.x), snap(greenSpot.trackingMarker.y)), 5, cv::Scalar(0xff, 0x00, 0xff), -1);
-        cv::circle(filtered, cv::Point2i(snap(redSpot.trackingMarker.x), snap(redSpot.trackingMarker.y)), 5, cv::Scalar(0x00, 0xff, 0xff), -1);
-        cv::circle(filtered, cv::Point2i(snap(yellowSpot.trackingMarker.x), snap(yellowSpot.trackingMarker.y)), 5, cv::Scalar(0x00, 0x00, 0xff), -1);
-        cv::circle(filtered, cv::Point2i(snap(blueSpot.trackingMarker.x), snap(blueSpot.trackingMarker.y)), 5, cv::Scalar(0xff, 0xff, 0x00), -1);
+        cv::circle(filtered, cv::Point2i(snap(yellowSpot.trackingMarker.x), snap(yellowSpot.trackingMarker.y)), 5, cv::Scalar(0x00, 0xff, 0xff), -1);
+        cv::circle(filtered, cv::Point2i(snap(orangeSpot.trackingMarker.x), snap(orangeSpot.trackingMarker.y)), 5, cv::Scalar(0xff, 0x80, 0x00), -1);
+        cv::circle(filtered, cv::Point2i(snap(blueSpot.trackingMarker.x), snap(blueSpot.trackingMarker.y)), 5, cv::Scalar(0x00, 0x00, 0xff), -1);
         // Convert back to ROS
         sensor_msgs::Image debug_image;
         cv_bridge::CvImage debug_converted(image->header, sensor_msgs::image_encodings::RGB8, filtered);
         debug_converted.toImageMsg(debug_image);
         debug_pub_.publish(debug_image);
-        ROS_INFO("Tracking operation finished");
+        //ROS_INFO("Tracking operation finished");
     }
 };
 
